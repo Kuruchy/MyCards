a9a98b0abc1dac48ac1e10a6b483bd488604863a
Bruno Retolaza <bruno.retolaza@gmail.com>
2021-10-23 17:04:58 +0200

    Fix Action FontSize

--- app/src/main/java/com/kurulabs/mycards/ui/composable/Action.kt
+++ app/src/main/java/com/kurulabs/mycards/ui/composable/Action.kt
@@ -3,6 +3,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
@@ -15,6 +16,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kurulabs.mycards.demo.getDemoActions
 import com.kurulabs.mycards.model.CardAction
 import com.kurulabs.mycards.ui.theme.MyCardsTheme
@@ -40,7 +42,7 @@ internal fun Action(
         Image(
             modifier = modifier
                 .width(width = 40.dp)
-                .padding(all = 2.dp)
+                .padding(all = 4.dp)
                 .align(alignment = Alignment.CenterVertically)
                 .wrapContentHeight(),
             contentScale = ContentScale.FillWidth,
@@ -48,12 +50,15 @@ internal fun Action(
             contentDescription = null
         )
 
-        Column {
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
 
             Text(
                 modifier = modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 8.dp)
                     .padding(top = 8.dp),
                 text = cardAction.title,
                 color = MaterialTheme.colors.onBackground,
@@ -63,11 +68,12 @@ internal fun Action(
             Text(
                 modifier = modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 8.dp)
+                    .padding(top = 2.dp)
                     .padding(bottom = 8.dp),
                 text = cardAction.subtitle,
                 color = MaterialTheme.colors.onBackground.copy(alpha = 0.3f),
-                textAlign = TextAlign.Start
+                textAlign = TextAlign.Start,
+                fontSize = 14.sp
             )
         }
     }
