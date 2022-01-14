b0d05eedffa67c315da72b53319ecf8d96612ce7
Bruno Retolaza <bruno.retolaza@gmail.com>
2022-01-14 19:45:01 +0100

    Remove unnecessary Composables

--- app/src/main/java/com/kurulabs/mycards/MainActivity.kt
+++ app/src/main/java/com/kurulabs/mycards/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
 import com.kurulabs.mycards.ui.composable.main.MainScreen
 import com.kurulabs.mycards.ui.state.CardViewModel
 import com.kurulabs.mycards.ui.theme.MyCardsTheme
@@ -20,12 +16,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyCardsTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background,
-                ) {
-                    MainScreen(viewModel, this)
-                }
+                MainScreen(viewModel, this)
             }
         }
     }
--- app/src/main/java/com/kurulabs/mycards/ui/composable/cards/CardOverview.kt
+++ app/src/main/java/com/kurulabs/mycards/ui/composable/cards/CardOverview.kt
@@ -10,40 +10,38 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.kurulabs.mycards.ui.state.CardViewModel
+import androidx.compose.ui.unit.dp
 import com.kurulabs.mycards.ui.composable.main.BottomNavigationHeight
-import com.kurulabs.mycards.ui.theme.MyCardsTheme
+import com.kurulabs.mycards.ui.state.CardViewModel
 
 @Composable
 fun CardOverview(
     viewModel: CardViewModel,
     context: Context
 ) {
-    MyCardsTheme {
-        BoxWithConstraints {
-            val maxHeight = maxHeight
-            val maxWidth = maxWidth
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = BottomNavigationHeight),
-                verticalArrangement = Arrangement.Top,
-            ) {
-                Carrousel(
-                    modifier = Modifier.height(maxHeight * 0.4f),
-                    cards = viewModel.cards,
-                    setActions = { index ->
-                        viewModel.setActions(index = index)
-                    }
-                )
-                Actions(
-                    modifier = Modifier.height(maxHeight * 0.6f),
-                    actions = viewModel.actions,
-                    onClick = { cardAction ->
-                        Toast.makeText(context, cardAction.name, Toast.LENGTH_SHORT).show()
-                    }
-                )
-            }
+    BoxWithConstraints {
+        val maxHeight = maxHeight
+        val maxWidth = maxWidth
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = BottomNavigationHeight),
+            verticalArrangement = Arrangement.Top,
+        ) {
+            Carrousel(
+                modifier = Modifier.height(maxHeight * 0.4f),
+                cards = viewModel.cards,
+                setActions = { index ->
+                    viewModel.setActions(index = index)
+                }
+            )
+            Actions(
+                modifier = Modifier.height(maxHeight * 0.6f).padding(horizontal = 16.dp),
+                actions = viewModel.actions,
+                onClick = { cardAction ->
+                    Toast.makeText(context, cardAction.name, Toast.LENGTH_SHORT).show()
+                }
+            )
         }
     }
 }
