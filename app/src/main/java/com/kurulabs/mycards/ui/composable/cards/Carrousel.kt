f4394097706bfe9fae26e7a1d2eb57ff12bc6ad6
Bruno Retolaza <bruno.retolaza@gmail.com>
2022-01-21 14:44:10 +0100

    Added Page indicator

--- app/src/main/java/com/kurulabs/mycards/ui/composable/cards/Carrousel.kt
+++ app/src/main/java/com/kurulabs/mycards/ui/composable/cards/Carrousel.kt
@@ -1,15 +1,18 @@
 package com.kurulabs.mycards.ui.composable.cards
 
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
 import com.google.accompanist.pager.rememberPagerState
 import com.kurulabs.mycards.ui.models.cards.CardData
 
@@ -24,18 +27,27 @@ internal fun Carrousel(
     BoxWithConstraints {
         val pagerState = rememberPagerState()
 
-        HorizontalPager(
-            modifier = modifier
-                .fillMaxWidth(),
-            count = cards.size,
-            state = pagerState,
-        ) { page ->
-            BankCard(
+        Column {
+            HorizontalPager(
+                modifier = modifier
+                    .fillMaxWidth(),
+                count = cards.size,
+                state = pagerState,
+            ) { page ->
+                BankCard(
+                    modifier = Modifier
+                        .aspectRatio(1.5f)
+                        .wrapContentHeight()
+                        .padding(horizontal = 24.dp),
+                    cardData = cards[page]
+                )
+            }
+
+            HorizontalPagerIndicator(
+                pagerState = pagerState,
                 modifier = Modifier
-                    .aspectRatio(1.5f)
-                    .wrapContentHeight()
-                    .padding(horizontal = 24.dp),
-                cardData = cards[page]
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp),
             )
         }
 
