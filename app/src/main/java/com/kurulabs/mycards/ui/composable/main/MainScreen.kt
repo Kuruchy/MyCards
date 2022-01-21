9728cc628efeb26e2cb171c705f5200062526522
Bruno Retolaza <bruno.retolaza@gmail.com>
2022-01-21 13:45:30 +0100

    Added Shimmering

--- app/src/main/java/com/kurulabs/mycards/ui/composable/main/MainScreen.kt
+++ app/src/main/java/com/kurulabs/mycards/ui/composable/main/MainScreen.kt
@@ -1,5 +1,14 @@
 package com.kurulabs.mycards.ui.composable.main
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Card
 import androidx.compose.material.DrawerValue
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberDrawerState
@@ -10,6 +19,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -17,8 +29,11 @@ import androidx.navigation.compose.rememberNavController
 import com.kurulabs.mycards.ui.composable.about.About
 import com.kurulabs.mycards.ui.composable.cards.ActionDetail
 import com.kurulabs.mycards.ui.composable.cards.CardOverview
+import com.kurulabs.mycards.ui.composable.cards.CardShimmerOverview
 import com.kurulabs.mycards.ui.models.main.BottomNavigationScreens
 import com.kurulabs.mycards.ui.state.CardViewModel
+import com.kurulabs.mycards.ui.theme.BankCardRoundedCornersShape
+import com.valentinilk.shimmer.shimmer
 import kotlinx.coroutines.flow.update
 
 private val DEFAULT_SCREEN = BottomNavigationScreens.Home
@@ -50,19 +65,23 @@ fun MainScreen(viewModel: CardViewModel) {
                 startDestination = DEFAULT_SCREEN.route
             ) {
                 composable(BottomNavigationScreens.Home.route) {
-                    CardOverview(
-                        cardsState = cardsState,
-                        onSwipe = { index -> carouselIndex = index },
-                        onActionClick = { cardAction ->
-                            viewModel.cardDetailState.update {
-                                it.copy(
-                                    cardData = cardsState.cards[carouselIndex],
-                                    cardAction = cardAction
-                                )
+                    if (cardsState.isLoading) {
+                        CardShimmerOverview()
+                    } else {
+                        CardOverview(
+                            cardsState = cardsState,
+                            onSwipe = { index -> carouselIndex = index },
+                            onActionClick = { cardAction ->
+                                viewModel.cardDetailState.update {
+                                    it.copy(
+                                        cardData = cardsState.cards[carouselIndex],
+                                        cardAction = cardAction
+                                    )
+                                }
+                                navController.navigate("Detail")
                             }
-                            navController.navigate("Detail")
-                        }
-                    )
+                        )
+                    }
                 }
                 composable("Detail") {
                     ActionDetail(
